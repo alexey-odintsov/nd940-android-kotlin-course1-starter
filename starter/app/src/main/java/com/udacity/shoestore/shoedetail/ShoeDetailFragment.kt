c8c443fe85b99d9827c9db84dc016d6642affe45
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2021-01-07 23:01:25 +0300

    Back on Cancel click

--- starter/app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeDetailFragmentBinding
 
@@ -15,10 +16,14 @@ class ShoeDetailFragment  : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding: ShoeDetailFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.shoe_detail_fragment, container, false
         )
+
+        binding.cancelButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         return binding.root
     }
